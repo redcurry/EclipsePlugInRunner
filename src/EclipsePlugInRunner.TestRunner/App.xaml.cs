71802459d4445645aab44f069087f1df39d14a56
Carlos Anderson <carlosja@med.umich.edu>
2019-07-24 14:03:22 -0400

    Use ESAPI 15.6 and configure to x64

--- src/EclipsePlugInRunner.TestRunner/App.xaml.cs
+++ src/EclipsePlugInRunner.TestRunner/App.xaml.cs
@@ -9,8 +9,12 @@ namespace EclipsePlugInRunner.TestRunner
     {
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            Patient patient = null;  // Dummy reference to workaround ESAPI bug
             ScriptRunner.Run(new Script());
         }
+
+        // Use an ESAPI object (Patient) so that ESAPI is referenced by this assembly
+        // (the dummy method is public so that it's not optimized away in a Release build);
+        // if ESAPI is not referenced, an exception is thrown by ESAPI
+        public void FixEsapiBug(Patient patient) { }
     }
 }
